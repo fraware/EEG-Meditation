9d970dcf9bfcccacaf998f2d4bc05fc0d726eda0
Matéo H. Petel <113530345+fraware@users.noreply.github.com>
2024-01-21 10:33:19 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 # Define constants and configurations
 FS = 250 # Sampling frequency
-FILE_PATH = r"C:\Users\mateo\OneDrive\Documents\GAP YEAR 2022-2023\Tech Internship Summer 2024\NextSense\EEG Project\earEEG_recording.TXT"
+FILE_PATH = r"earEEG_recording.TXT"
 CHANNEL = '6' # The channel to be analyzed
 
 # Define frequency bands
